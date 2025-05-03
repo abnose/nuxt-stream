4d9e012b2b82281018cd9c6d1b7099a9660433e2
e.karimi <abnoseek@gmail.com>
2025-05-03 13:29:10 +0330

    feat: add navbar

---
+++ components/base/Navbar.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="card">
+    <Menubar :model="items">
+      <template #start>
+        <!-- Logo -->
+
+        <Button
+          icon="pi pi-bars"
+          severity="primary"
+          variant="text"
+          rounded
+          raised
+          @click="visible = true"
+        />
+      </template>
+      <template #item="{ item, props, hasSubmenu, root }">
+        <a v-ripple class="flex items-center" v-bind="props.action">
+          <span>{{ item.label }}</span>
+          <Badge
+            v-if="item.badge"
+            :class="{ 'ml-auto': !root, 'ml-2': root }"
+            :value="item.badge"
+          />
+          <span
+            v-if="item.shortcut"
+            class="ml-auto border border-surface rounded bg-emphasis text-muted-color text-xs p-1"
+            >{{ item.shortcut }}</span
+          >
+          <i
+            v-if="hasSubmenu"
+            :class="[
+              'pi pi-angle-down ml-auto',
+              { 'pi-angle-down': root, 'pi-angle-right': !root },
+            ]"
+          ></i>
+        </a>
+      </template>
+      <template #end>
+        <div class="flex items-center gap-2 cursor-pointer">
+          <InputText
+            placeholder="Search"
+            type="text"
+            class="w-[25em] sm:w-auto"
+          />
+          <Avatar
+            @click="toggle"
+            image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png"
+            shape="circle"
+          />
+        </div>
+      </template>
+    </Menubar>
+
+    <Popover ref="op">
+      <ul class="list-none p-0 m-0 flex flex-col gap-1">
+        <li
+          v-for="item in navItems"
+          :key="item.name"
+          class="flex items-center w-[15.5em] text-sm cursor-pointer"
+        >
+          <div
+            class="flex w-full justify-between items-center p-2 hover:bg-gray-100 rounded-xl"
+          >
+            <span class="font-medium">{{ item.name }}</span>
+            <i :class="`pi ${item.icon}`"></i>
+          </div>
+        </li>
+      </ul>
+    </Popover>
+    <div class="card flex justify-center">
+      <Drawer
+        v-model:visible="visible"
+        header="Drawer"
+        class="!w-full md:!w-80 lg:!w-[30rem]"
+      >
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </Drawer>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const visible = ref(false);
+const op = ref();
+const toggle = (event) => {
+  op.value.toggle(event);
+};
+const items = ref([
+  {
+    label: "Home",
+    icon: "pi pi-home",
+  },
+]);
+const navItems = ref([
+  {
+    name: "Dashboard",
+    icon: "pi-cog",
+  },
+  {
+    name: "Logout",
+    icon: "pi-sign-out",
+  },
+]);
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
+    <Navbar />
+
     <Toast />
     <NuxtPage />
   </div>
 </template>
+<script setup>
+import Navbar from "@/components/base/Navbar.vue";
+</script>
